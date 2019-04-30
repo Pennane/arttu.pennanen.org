285b2a8b4d8ae663f2ba72077deeee90b7b91604
Arttu Pennanen <arttu@pennanen.org>
2019-04-30 18:56:10 +0300

    Fixed parallax to load after content load

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
         try {
             navigator.serviceWorker.register('/sw.js')
-        } catch(err) {
+        } catch (err) {
             console.log('ServiceWorker registration failed: ', err);
         }
     });
-  }
+}
 
 
 const path = '/assets/content/content.json'
@@ -19,29 +19,29 @@ const newEl = (type, classes) => {
 }
 
 const createProject = (data, target) => {
-    let a = newEl('a', ["flex","project-div"])
-    a.setAttribute("id",  data.name)
+    let a = newEl('a', ["flex", "project-div"])
+    a.setAttribute("id", data.name)
     a.setAttribute("href", data.href)
     a.setAttribute("title", data.name)
 
-            let cover = newEl('div', ["project"])
-                    let header =  newEl('h3', data.whiteText ? ["project-header invertable"] :  ["project-header"]) 
-                    header.textContent = data.name
-                    cover.appendChild(header)
+    let cover = newEl('div', ["project"])
+    let header = newEl('h3', data.whiteText ? ["project-header invertable"] : ["project-header"])
+    header.textContent = data.name
+    cover.appendChild(header)
 
-                    let bgOuter = newEl('div', ["project-bg"])
-                            let bgInner = newEl('div', data.invert ? ["project-bg invert"] :  ["project-bg"])
-                            bgInner.style.backgroundImage = `url(${data.imgUrl})`
-                            bgOuter.appendChild(bgInner)
-                    cover.appendChild(bgOuter)
-            a.appendChild(cover)
+    let bgOuter = newEl('div', ["project-bg"])
+    let bgInner = newEl('div', data.invert ? ["project-bg invert"] : ["project-bg"])
+    bgInner.style.backgroundImage = `url(${data.imgUrl})`
+    bgOuter.appendChild(bgInner)
+    cover.appendChild(bgOuter)
+    a.appendChild(cover)
 
-            let meta = newEl('div', ["project-meta"])
-                    let description = newEl('p', ["project-meta"])
-                    description.textContent = data.description
-                    meta.appendChild(description)
-            a.appendChild(meta)
-            
+    let meta = newEl('div', ["project-meta"])
+    let description = newEl('p', ["project-meta"])
+    description.textContent = data.description
+    meta.appendChild(description)
+    a.appendChild(meta)
+
     target.appendChild(a)
 }
 
@@ -64,6 +64,18 @@ loadJSON(path, data => {
         createProject(project, wrapperElement)
     });
     projectTarget.appendChild(wrapperElement)
+    let instance = basicScroll.create({
+        elem: document.querySelector('body'),
+        from: 'top-top',
+        to: 'bottom-bottom',
+        props: {
+            '--animationparallax': {
+                from: `-${parseInt(document.body.scrollHeight / 10)}px`,
+                to: '0px',
+            }
+        }
+    })
+    instance.start()
 })
 
 let animationTargetEl = document.querySelector(".animationparent")
@@ -75,60 +87,49 @@ const getAmount = () => 20 + Math.floor((window.innerWidth / 100) * 2)
 let currentAmount = getAmount(window.innerWidth)
 
 function createBubbles(amount, target) {
-	amount = amount > 100 ? 100 : amount;
-	let old = document.querySelector(".heroanimator");
-	let bubbleParent;
-	if (old) {
-		old.innerHTML = "";
-		bubbleParent = old;
-	} else {
-		bubbleParent = document.createElement('div')
-		bubbleParent.className = "heroanimator"
-	}
-	let duration = 400;
-	for (let i = 0; i < amount; i++) {
+    amount = amount > 100 ? 100 : amount;
+    let old = document.querySelector(".heroanimator");
+    let bubbleParent;
+    if (old) {
+        old.innerHTML = "";
+        bubbleParent = old;
+    } else {
+        bubbleParent = document.createElement('div')
+        bubbleParent.className = "heroanimator"
+    }
+    let duration = 400;
+    for (let i = 0; i < amount; i++) {
         let el = document.createElement('div')
         let rn = Math.random() + 0.1
         let nr = rnd(3, 97).toFixed(0)
         el.style.left = `${nr}%`;
-        el.style.transform = `scale(${rn.toFixed(2)}) translateX(-50%)`; 
-		el.className = "bubble";
-		el.style.animationDelay = `${(-(i * duration * (Math.random() * 2))).toFixed(0)}s`;
-		el.style.animationDuration = `${(20 + (duration * (rn))).toFixed(0)}s`
-		bubbleParent.appendChild(el)
-	}
-	target.appendChild(bubbleParent)
+        el.style.transform = `scale(${rn.toFixed(2)}) translateX(-50%)`;
+        el.className = "bubble";
+        el.style.animationDelay = `${(-(i * duration * (Math.random() * 2))).toFixed(0)}s`;
+        el.style.animationDuration = `${(20 + (duration * (rn))).toFixed(0)}s`
+        bubbleParent.appendChild(el)
+    }
+    target.appendChild(bubbleParent)
 }
 
 function initBubbles() {
-	let amount = getAmount()
-	createBubbles(amount, animationTargetEl)
+    let amount = getAmount()
+    createBubbles(amount, animationTargetEl)
 }
 
 let timeout;
 window.addEventListener("resize", event => {
-	clearTimeout(timeout)
-	timeout = setTimeout(() => {
-		let changedAmount = getAmount();
-		if (Math.abs(currentAmount - changedAmount ) > 20 ) {
-			initBubbles()
-			currentAmount = changedAmount;
-		}
-		
-	}, 400)
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+        let changedAmount = getAmount();
+        if (Math.abs(currentAmount - changedAmount) > 20) {
+            initBubbles()
+            currentAmount = changedAmount;
+        }
+
+    }, 400)
 })
 
 initBubbles()
 
-let instance = basicScroll.create({
-        elem: document.querySelector('body'),
-        from: '0px',
-        to: `${document.body.scrollHeight}px`,
-        props: {
-            '--animationparallax': {
-                from: `-${parseInt(document.body.scrollHeight / 10)}px`,
-                to: '0px',
-            }
-        }
-    })
-instance.start()
+
