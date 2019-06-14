e7f6b8ee66548e9161462b183dd1532e3df8748a
Arttu Pennanen <arttu@pennanen.org>
2019-06-15 02:32:07 +0300

    Trying to foce sw to remove old caches on update.

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-var version = "1.3.1"
-var CACHE_NAME = 'arttu-pennanen-org-cache-v' + version;
-var urlsToCache = [
-    '/',
-    './css/default.css',
-    './js/main.js',
-    './js/basicScroll.min.js',
-    './assets/font/inter/Inter-Regular.woff2',
-    './assets/font/inter/Inter-Medium.woff2',
-    './assets/font/inter/Inter-Bold.woff2',
-    './assets/font/inter/Inter-ExtraBold.woff2'
-];
-
+var version = "1.3.3"
+var CACHE_NAME = `arttu-pennanen-org-cache-v${version}`;
+var urlsToCache = ['/', './css/default.css', './js/main.js', './js/basicScroll.min.js', './assets/font/inter/Inter-Regular.woff2', './assets/font/inter/Inter-Medium.woff2', './assets/font/inter/Inter-Bold.woff2', './assets/font/inter/Inter-ExtraBold.woff2'];
 var contentPath = '/assets/content/content.json'
 
 function loadJSON(path, callback) {
@@ -22,7 +12,6 @@ function loadJSON(path, callback) {
         .catch(() => { null })
 }
 
-
 loadJSON(contentPath, data => {
     data.projects.forEach(project => {
         urlsToCache.push(project.imgUrl)
@@ -30,56 +19,41 @@ loadJSON(contentPath, data => {
 })
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+    event.waitUntil(caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(urlsToCache);
+        }))
 });
 
 self.addEventListener('fetch', (event) => {
     if (event.request.url.match('\/sub\/?$')) {
         return false;
     }
-
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-
-                return fetch(event.request).then(
-                    (response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-
+    event.respondWith(caches.match(event.request)
+        .then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request)
+                .then((response) => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
                         return response;
                     }
-                );
-            })
-    );
+                    var responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
+        }))
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(CACHE_NAME) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('activate', event => {
+    event.waitUntil(caches.keys()
+        .then(keys => Promise.all(keys.map(key => {
+            if (key !== CACHE_NAME) {
+                return caches.delete(key);
+            }
+        }))))
 });
