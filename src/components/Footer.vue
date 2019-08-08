ffe80973a64e09631c6145eb24b6c567f5fb847e
Arttu Pennanen <arttu@pennanen.org>
2019-08-08 18:27:38 +0300

    Moving to Vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer id="footer">
+    <span>Vissii joku footer</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
---
+++ src/views/projects/peigombot.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="project">
+    <div class="head">
+      <h2>Typechase</h2>
+      <a class="tryhere" href="https://arttu.pennanen.org/sub/typechase">Try here!</a>
+    </div>
+    <figure class="projectimg">
+      <img src="assets/images/typechase1.png" alt />
+      <figcaption>arttu.pennanen.org/typechase</figcaption>
+    </figure>
+
+    <figure class="projectimg">
+      <img src="assets/images/typechase2.png" alt />
+      <figcaption>arttu.pennanen.org/typechase</figcaption>
+    </figure>
+    <figure class="projectimg">
+      <img src="assets/images/typechase3.png" alt />
+      <figcaption>arttu.pennanen.org/typechase</figcaption>
+    </figure>
+  </div>
+</template>
