cb3e86c02b0c6e77ec4b033b03a72b4097150108
Arttu Pennanen <arttu@pennanen.org>
2019-08-08 21:26:12 +0300

    Placeholder content for projects

--- src/views/projects/peigombot.vue
+++ src/views/projects/peigombot.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="project">
     <div class="head">
-      <h2>Typechase</h2>
-      <a class="tryhere" href="https://arttu.pennanen.org/sub/typechase">Try here!</a>
+      <h2>Peigom bot</h2>
+      <a class="tryhere" href="https://github.com/Pennane/peigom-bot">Find here!</a>
     </div>
-    <figure class="projectimg">
-      <img src="assets/images/typechase1.png" alt />
-      <figcaption>arttu.pennanen.org/typechase</figcaption>
-    </figure>
-
-    <figure class="projectimg">
-      <img src="assets/images/typechase2.png" alt />
-      <figcaption>arttu.pennanen.org/typechase</figcaption>
-    </figure>
-    <figure class="projectimg">
-      <img src="assets/images/typechase3.png" alt />
-      <figcaption>arttu.pennanen.org/typechase</figcaption>
-    </figure>
+    <p>A discord bot</p>
   </div>
 </template>
