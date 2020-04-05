755823c7a9fa1af4f5610d21b78cc868cebb7fad
Arttu Pennanen <arttu@pennanen.org>
2020-04-06 01:00:39 +0300

    Push other old projects

--- public/sub/60seconds/js/counter.js
+++ public/sub/60seconds/js/counter.js
@@ -4,16 +4,16 @@ let timeEl = document.getElementById("time")
 let textEl = document.getElementById("text")
 let now = Date.now()
 
-let hash = parseFloat(window.location.hash.replace("#","")) || false;
+let hash = parseFloat(window.location.hash.replace("#", "")) || false;
 let base = 60;
 
 let time = hash ? hash * base : base;
 let end = new Date(Date.now() + time * 1000)
-	
+
 textEl.textContent = "seconds."
 timeEl.textContent = parseInt((end - now) / 1000)
-	
-let loop = setInterval(()=>{
+
+let loop = setInterval(() => {
 	let now = Date.now()
 	let s = parseInt((end - now) / 1000)
 	if (end > now) {
