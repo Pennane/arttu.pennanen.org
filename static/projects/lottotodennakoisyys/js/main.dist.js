b05447172e773a0d14fd16ba8d50e7e2f8d83a78
Arttu Pennanen <arttu@pennanen.org>
2021-02-15 04:27:12 +0200

    Update lottery simulator

--- static/projects/lottotodennakoisyys/js/main.dist.js
+++ static/projects/lottotodennakoisyys/js/main.dist.js
@@ -1,4 +1,46 @@
-let statistics = {
+function _toConsumableArray(arr) {
+  return (
+    _arrayWithoutHoles(arr) ||
+    _iterableToArray(arr) ||
+    _unsupportedIterableToArray(arr) ||
+    _nonIterableSpread()
+  );
+}
+
+function _nonIterableSpread() {
+  throw new TypeError(
+    "Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."
+  );
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n))
+    return _arrayLikeToArray(o, minLen);
+}
+
+function _iterableToArray(iter) {
+  if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter))
+    return Array.from(iter);
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+  for (var i = 0, arr2 = new Array(len); i < len; i++) {
+    arr2[i] = arr[i];
+  }
+  return arr2;
+}
+
+var statistics = {
   v0: {
     payout: 0,
     amount: 0,
@@ -56,8 +98,7 @@ let statistics = {
     probability: 0
   }
 };
-
-let lotteryNumberElements = [
+var lotteryNumberElements = [
   document.getElementById("lspan1"),
   document.getElementById("lspan2"),
   document.getElementById("lspan3"),
@@ -66,8 +107,7 @@ let lotteryNumberElements = [
   document.getElementById("lspan6"),
   document.getElementById("lspan7")
 ];
-
-let statisticsElements = {
+var statisticsElements = {
   "3+1": {
     amount: document.getElementById("v3ba"),
     probability: document.getElementById("v3bp"),
@@ -107,38 +147,55 @@ let statisticsElements = {
   }
 };
 
-const displayWinningNumbers = numbers => {
-  let bonus = numbers[7];
-  let nums = numbers.slice(0, 7).sort((a, b) => a - b);
-  console.log(`Lottonumerot: ${nums}`);
-  console.log(`Lisänumero: ${bonus}`);
+var displayWinningNumbers = function displayWinningNumbers(numbers) {
+  var bonus = numbers[7];
+  var nums = numbers.slice(0, 7).sort(function(a, b) {
+    return a - b;
+  });
+  console.log("Lottonumerot: ".concat(nums));
+  console.log("Lis\xE4numero: ".concat(bonus));
 };
 
-const groupInt = int => {
-  if (int === Infinity || int === 0 || int < 999) return int;
-  int = parseInt(int);
-  let skip = ["+", "e", ".", ","];
-  let strInt = int.toString();
-  let groupIndex = 1;
-  let toReturn = "";
-  for (let i = strInt.length; i > 0; i--) {
+var groupInt = function groupInt(_int) {
+  if (_int === Infinity || _int === 0 || _int < 999) return _int;
+  _int = parseInt(_int);
+  var skip = ["+", "e", ".", ","];
+
+  var strInt = _int.toString();
+
+  var groupIndex = 1;
+  var toReturn = "";
+
+  for (var i = strInt.length; i > 0; i--) {
     toReturn = strInt[i - 1] + toReturn + "";
+
     if (skip.indexOf(strInt[i - 1]) > -1) {
       groupIndex = 1;
       continue;
     }
+
     if (groupIndex % 3 === 0) {
       toReturn = " " + toReturn;
     }
+
     groupIndex++;
   }
+
   return toReturn;
 };
 
-const generateNumbers = (amount = 7, highest = 40, lowest = 1) => {
-  let numbers = [];
+var generateNumbers = function generateNumbers() {
+  var amount =
+    arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 7;
+  var highest =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 40;
+  var lowest =
+    arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
+  var numbers = [];
+
   do {
-    let n = Math.floor(Math.random() * highest) + lowest;
+    var n = Math.floor(Math.random() * highest) + lowest;
+
     if (numbers.indexOf(n) === -1) {
       numbers.push(n);
     }
@@ -147,12 +204,14 @@ const generateNumbers = (amount = 7, highest = 40, lowest = 1) => {
   return numbers;
 };
 
-const getHowManyCorrect = numbers => {
-  let correctNumbers = 0;
-  let bonus = false;
-  for (let i = 0; i < 7; i++) {
+var getHowManyCorrect = function getHowManyCorrect(numbers) {
+  var correctNumbers = 0;
+  var bonus = false;
+
+  for (var i = 0; i < 7; i++) {
     if (numbers.indexOf(winningNumbers[i]) !== -1) correctNumbers++;
   }
+
   if (numbers.indexOf(winningNumbers[7]) !== -1) bonus = true;
   return {
     correct: correctNumbers,
@@ -160,54 +219,60 @@ const getHowManyCorrect = numbers => {
   };
 };
 
-const purchaseTickets = (amount = 1) => {
-  for (let i = 0; i < amount; i++) {
-    let numbers = generateNumbers();
-    let correctNumbers = getHowManyCorrect(numbers);
+var purchaseTickets = function purchaseTickets() {
+  var amount =
+    arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+
+  for (var i = 0; i < amount; i++) {
+    var numbers = generateNumbers();
+    var correctNumbers = getHowManyCorrect(numbers);
     updateStatistics(correctNumbers);
+
     if (i === amount - 1) {
-      purchasedRow = numbers.sort((a, b) => a - b);
+      purchasedRow = numbers.sort(function(a, b) {
+        return a - b;
+      });
       updateDom();
     }
   }
 };
 
-const purchaseInfinitely = () => {
-  let domTarget = document.getElementById("purchaseinfiniteticket");
+var purchaseInfinitely = function purchaseInfinitely() {
+  var domTarget = document.getElementById("purchaseinfiniteticket");
+
   if (purchasingInfinitely === false) {
     purchasingInfinitely = true;
-
     domTarget.textContent = "Pysäytä rivien osto";
     domTarget.classList.toggle("active", true);
-
-    let w7n = statistics.v7.amount;
-
-    window.lotteryloop = setInterval(() => {
+    var w7n = statistics.v7.amount;
+    window.lotteryloop = setInterval(function() {
       purchaseTickets(1000);
+
       if (w7n !== statistics.v7.amount) {
         clearInterval(lotteryloop);
       }
     }, 40);
   } else if (purchasingInfinitely === true) {
     purchasingInfinitely = false;
-
     domTarget.textContent = "Osta loputtomasti rivejä";
     domTarget.classList.toggle("active", false);
-
     clearInterval(lotteryloop);
   }
 };
 
-const updateStatistics = ({ correct, bonus }) => {
+var updateStatistics = function updateStatistics(_ref) {
+  var correct = _ref.correct,
+    bonus = _ref.bonus;
   statistics.total.amount++;
 
   if (bonus === true) {
     statistics.total.bonus++;
   }
 
-  let stat =
-    bonus && (correct === 3 || correct === 6) ? `v${correct}b` : `v${correct}`;
-
+  var stat =
+    bonus && (correct === 3 || correct === 6)
+      ? "v".concat(correct, "b")
+      : "v".concat(correct);
   statistics[stat].amount++;
 
   if ((correct === 3 && bonus) || correct > 3) {
@@ -237,11 +302,10 @@ const updateStatistics = ({ correct, bonus }) => {
   );
 };
 
-const updateDom = () => {
-  lotteryNumberElements.forEach((el, i) => {
+var updateDom = function updateDom() {
+  lotteryNumberElements.forEach(function(el, i) {
     el.textContent = purchasedRow[i];
   });
-
   statisticsElements["3+1"].amount.textContent = groupInt(
     statistics.v3b.amount
   );
@@ -250,13 +314,11 @@ const updateDom = () => {
   statisticsElements["3+1"].winnings.textContent = groupInt(
     statistics.v3b.amount * statistics.v3b.payout
   );
-
   statisticsElements["4"].amount.textContent = groupInt(statistics.v4.amount);
   statisticsElements["4"].probability.textContent = statistics.v4.probability;
   statisticsElements["4"].winnings.textContent = groupInt(
     statistics.v4.amount * statistics.v4.payout
   );
-
   statisticsElements["5"].amount.textContent = groupInt(statistics.v5.amount);
   statisticsElements["5"].probability.textContent = groupInt(
     statistics.v5.probability
@@ -264,7 +326,6 @@ const updateDom = () => {
   statisticsElements["5"].winnings.textContent = groupInt(
     statistics.v5.amount * statistics.v5.payout
   );
-
   statisticsElements["6"].amount.textContent = groupInt(statistics.v6.amount);
   statisticsElements["6"].probability.textContent = groupInt(
     statistics.v6.probability
@@ -272,7 +333,6 @@ const updateDom = () => {
   statisticsElements["6"].winnings.textContent = groupInt(
     statistics.v6.amount * statistics.v6.payout
   );
-
   statisticsElements["6+1"].amount.textContent = groupInt(
     statistics.v6b.amount
   );
@@ -282,7 +342,6 @@ const updateDom = () => {
   statisticsElements["6+1"].winnings.textContent = groupInt(
     statistics.v6b.amount * statistics.v6b.payout
   );
-
   statisticsElements["7"].amount.textContent = groupInt(statistics.v7.amount);
   statisticsElements["7"].probability.textContent = groupInt(
     statistics.v7.probability
@@ -290,7 +349,6 @@ const updateDom = () => {
   statisticsElements["7"].winnings.textContent = groupInt(
     statistics.v7.amount * statistics.v7.payout
   );
-
   statisticsElements["total"].rows.textContent = groupInt(
     statistics.total.amount
   );
@@ -312,14 +370,16 @@ const updateDom = () => {
   );
 };
 
-let purchasedRow = [];
-let purchasingInfinitely = false;
-let winningNumbers = generateNumbers(8);
-
+var purchasedRow = [];
+var purchasingInfinitely = false;
+var winningNumbers = generateNumbers(8);
 displayWinningNumbers(winningNumbers);
 
-[...document.querySelectorAll(".fraction")].forEach(el => {
-  let split = el.innerHTML.split("/");
+_toConsumableArray(document.querySelectorAll(".fraction")).forEach(function(
+  el
+) {
+  var split = el.innerHTML.split("/");
+
   if (split.length === 2) {
     el.innerHTML =
       '<span class="top">' +
@@ -330,20 +390,28 @@ displayWinningNumbers(winningNumbers);
   }
 });
 
-document.getElementById("purchase1ticket").addEventListener("click", () => {
-  purchaseTickets(1);
-});
-document.getElementById("purchase10ticket").addEventListener("click", () => {
-  purchaseTickets(10);
-});
-document.getElementById("purchase100ticket").addEventListener("click", () => {
-  purchaseTickets(100);
-});
-document.getElementById("purchase1000ticket").addEventListener("click", () => {
-  purchaseTickets(1000);
-});
+document
+  .getElementById("purchase1ticket")
+  .addEventListener("click", function() {
+    purchaseTickets(1);
+  });
+document
+  .getElementById("purchase10ticket")
+  .addEventListener("click", function() {
+    purchaseTickets(10);
+  });
+document
+  .getElementById("purchase100ticket")
+  .addEventListener("click", function() {
+    purchaseTickets(100);
+  });
+document
+  .getElementById("purchase1000ticket")
+  .addEventListener("click", function() {
+    purchaseTickets(1000);
+  });
 document
   .getElementById("purchaseinfiniteticket")
-  .addEventListener("click", () => {
+  .addEventListener("click", function() {
     purchaseInfinitely();
   });
